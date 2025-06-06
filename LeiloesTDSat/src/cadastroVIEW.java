97003c8adf8ead49b18a96cf8ca867d1e9fdf9a0
Andre Lupercinio <andr.lupercinio@gmail.com>
2025-06-05 21:58:20 -0300

    Revert "Inclusão dos construtores"
    This reverts commit 4ab6b03fc7301262d81241cd1475c76c76ed7d4b.

--- LeiloesTDSat/src/cadastroVIEW.java
+++ LeiloesTDSat/src/cadastroVIEW.java
@@ -1,12 +1,8 @@
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+
 /**
  *
  * @author Adm
@@ -139,43 +135,26 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cadastroNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastroNomeActionPerformed
-
-
+        
+        
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
         ProdutosDTO produto = new ProdutosDTO();
-
         String nome = cadastroNome.getText();
-        int valor = Integer.parseInt(cadastroValor.getText());
+        String valor = cadastroValor.getText();
         String status = "A Venda";
         produto.setNome(nome);
-        produto.setValor(valor);
+        produto.setValor(Integer.parseInt(valor));
         produto.setStatus(status);
-
-        ProdutosDAO produtodao = new ProdutosDAO();
-        boolean sucesso = false;
         
-        try {
-            sucesso = produtodao.cadastrarProduto(produto);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(cadastroVIEW.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        if (sucesso) {
-            
-            JOptionPane.showMessageDialog(this, "Produto cadastrado com sucesso!");
-            cadastroNome.setText("");
-            cadastroValor.setText("");
-
-        } else {
-            JOptionPane.showMessageDialog(this, "Erro ao cadastrar produto.");
-        }
-
+        ProdutosDAO produtodao = new ProdutosDAO();
+        produtodao.cadastrarProduto(produto);
+        
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
-        listagemVIEW listagem = new listagemVIEW();
+        listagemVIEW listagem = new listagemVIEW(); 
         listagem.setVisible(true);
     }//GEN-LAST:event_btnProdutosActionPerformed
 
--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -4,6 +4,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,44 +16,18 @@ import javax.swing.JOptionPane;
  * @author Adm
  */
 public class conectaDAO {
-
-    private Connection conn;
-
-    public Connection connectDB() throws ClassNotFoundException {
-
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
         try {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=Alrm100694.");
-            System.out.println("Conectou");
-
-        } catch (SQLException erro) {
+        
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            
+        } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
-
         return conn;
     }
     
-    public void desconectar() {
-
-        try {
-            
-            if (conn != null && !conn.isClosed()){
-                
-                conn.close();
-                System.out.println("Desconectado");
-            
-            }
-        } catch (SQLException se) {
-            
-            System.out.println("Problema ao desconectar do banco." + se);
-
-        }
-        
-    }
-
-    public Connection getConn() {
-        return conn;
-    }
-
 }
