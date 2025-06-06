af8423e1cc75c9663189efaf5186e3780de72348
Andre Lupercinio <andr.lupercinio@gmail.com>
2025-06-05 22:25:53 -0300

    Criando método de salvar produto e mensagem de confirmação de cadastro de produto

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,12 +23,31 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public static boolean cadastrarProduto(ProdutosDTO produto) throws ClassNotFoundException, SQLException {
+
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+        try {
+            conectaDAO conn = new conectaDAO();
+            conn.connectDB();
+
+            PreparedStatement ps = conn.getConn().prepareStatement(sql);
+
+            ps.setString(1, produto.getNome());
+            ps.setInt(2, produto.getValor());
+            ps.setString(3, produto.getStatus());
+
+            int inserirTabela = ps.executeUpdate();
+            System.out.println("Produto cadastrado com sucesso");
+            conn.desconectar();
+            return inserirTabela > 0;
+
+        } catch (SQLException se) {
+
+            System.err.println("erro ao cadastrar produto: " + se.getMessage());
+            return false;
+        }
+
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
